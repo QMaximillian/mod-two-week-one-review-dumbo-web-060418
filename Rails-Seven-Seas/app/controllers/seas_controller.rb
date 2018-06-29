e0fe81cefcf87a651131e9250a5f8112a180e537
QMaximillian <quinnlashinsky@gmail.com>
2018-06-29 17:44:03 -0400

    Built sinatra app in rails

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,37 @@
 class SeasController < ApplicationController
   #define your controller actions here
+  def index
+  	@seas = Sea.all
+  	render :index
+  end
 
+  def new
+  	@sea = Sea.new
+  	render :new
+  end
+
+  def edit
+  	@sea = Sea.find(params[:id])
+  	render :edit
+  end
+
+  def show 
+  	@sea = Sea.find(params[:id])
+  	render :show
+  end
+
+  def update
+  	@sea = Sea.find(params[:id])
+  	@sea.update(sea_params)
+
+  	redirect_to "/seas/#{@sea.id}"
+  end
+
+  def create
+  	@sea = Sea.create(sea_params)
+
+  	redirect_to "/seas/#{@sea.id}"
+  end
 
   private
   # In controller actions, use this private method to access sea params from forms.
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :seas, only: [:new, :edit, :show, :update, :create, :index]
+  # get '/seas', to: 'seas#index'
+  # get '/seas/new', to: 'seas#new'
+  # get '/seas/:id', to: 'seas#show', as: 'sea'
+  # get '/seas/:id/edit', to: 'seas#edit'
+
+  # post '/seas', to: 'seas#create'
+  # resources :seas
+  
+  get '/welcome', to: 'seas#welcome'
 end
